11f8185862af2ded2cb304ce62ee3a890cc118d6
Nexeum <juaandhe@bancolombia.com.co>
2024-04-18 12:52:07 -0500

    fix(OrderService.java): change name class

--- order-ms/src/main/java/com/nexeum/orderms/service/OrderService.java
+++ order-ms/src/main/java/com/nexeum/orderms/service/OrderService.java
@@ -2,7 +2,7 @@ package com.nexeum.orderms.service;
 
 import org.springframework.http.ResponseEntity;
 
-public interface OrderController {
+public interface OrderService {
     ResponseEntity<?> createOrder();
     ResponseEntity<?> cancelOrder();
     ResponseEntity<?> getAllOrders();
