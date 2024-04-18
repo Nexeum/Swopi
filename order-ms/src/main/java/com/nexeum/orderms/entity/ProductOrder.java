d2075ea0432e15a30706c32130076e9ee68dd67a
Nexeum <juaandhe@bancolombia.com.co>
2024-04-18 13:10:57 -0500

    feat: Add OrderItem and ProductOrder entities

---
+++ order-ms/src/main/java/com/nexeum/orderms/entity/ProductOrder.java
@@ -0,0 +1,36 @@
+package com.nexeum.orderms.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+public class ProductOrder {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String cartId;
+    private BigDecimal totalOrderPrice;
+    private String orderStatus;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems = new ArrayList<>();
+    private Date createdDate;
+    private Date updatedDate;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdDate = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedDate = new Date();
+    }
+}
