9d653f3e7bd555d95b8dede8c3e194549bb1f5c0
Nexeum <juaandhe@bancolombia.com.co>
2024-04-18 18:31:03 -0500

    fix(ProductService.java): add @Service annotation to ProductService

--- product-ms/src/main/java/com/nexeum/productms/service/ProductService.java
+++ product-ms/src/main/java/com/nexeum/productms/service/ProductService.java
@@ -1,10 +1,12 @@
 package com.nexeum.productms.service;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
 
+@Service
 public interface ProductService {
     ResponseEntity<?> addProduct(MultipartFile imageFile, String name, String description, String brandName,
                                  BigDecimal pricePerUnit, BigDecimal productWholeSalePrice, Long noOfStocks);
