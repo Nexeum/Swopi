3cfb55d723537590c538576909fb68a470a93102
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 15:09:35 -0500

    fix(Product): configure entity, service

--- product-ms/src/main/java/com/nexeum/productms/entity/Product.java
+++ product-ms/src/main/java/com/nexeum/productms/entity/Product.java
@@ -1,17 +1,12 @@
 package com.nexeum.productms.entity;
 
-import jakarta.persistence.*;
 import lombok.Data;
-
 import java.math.BigDecimal;
-@Entity
+
 @Data
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Column(columnDefinition = "Text")
     private String description;
     private String brandName;
     private BigDecimal pricePerUnit;
--- product-ms/src/main/java/com/nexeum/productms/service/ProductService.java
+++ product-ms/src/main/java/com/nexeum/productms/service/ProductService.java
@@ -4,14 +4,12 @@ import com.nexeum.productms.dto.response.ServiceResponse;
 import com.nexeum.productms.entity.Product;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.codec.multipart.FilePart;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 
 
-@Service
 public interface ProductService {
     Mono<ResponseEntity<ServiceResponse>> addProduct(Mono<FilePart> imageFile, String name, String description, String brandName,
                                                      BigDecimal pricePerUnit, BigDecimal productWholeSalePrice, Long noOfStocks);
