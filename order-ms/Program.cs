5b67f431bd38c38834f82f4f1b2fabff53d4426b
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 16:42:50 -0500

    fix(Program.cs): add controllers, configure context and add scoped Repository

--- order-ms/Program.cs
+++ order-ms/Program.cs
@@ -1,12 +1,20 @@
 using order_ms.service;
+using order_ms.repository;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers(); // Add this line
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Configure DbContext
+builder.Services.AddDbContext<ProductOrderRepository>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Add your services and repositories to the DI container
 builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<ProductOrderRepository>();
 
 var app = builder.Build();
 
