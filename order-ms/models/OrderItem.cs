f9ea6ca007d5f409fa3d6161fe627d0b1f0b348f
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 16:15:36 -0500

    fix(models): resolve null values

--- order-ms/models/OrderItem.cs
+++ order-ms/models/OrderItem.cs
@@ -1,8 +1,7 @@
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace OrderMs.Models
+namespace order_ms.models
 {
     public class OrderItem
     {
@@ -10,11 +9,11 @@ namespace OrderMs.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         public long ProductId { get; set; }
-        public string ProductName { get; set; }
+        public string? ProductName { get; set; } = string.Empty;
         public long ProductQuantity { get; set; }
         public decimal TotalPrice { get; set; }
         public decimal PricePerUnit { get; set; }
-        public string ProductImageUrl { get; set; }
+        public string? ProductImageUrl { get; set; } = string.Empty;
 
         [ForeignKey("OrderId")]
         public ProductOrder Order { get; set; }
