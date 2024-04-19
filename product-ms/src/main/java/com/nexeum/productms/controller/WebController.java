2c80ba19c40817dcd17186a627f2962d46615ba3
Nexeum <juaandhe@bancolombia.com.co>
2024-04-18 19:08:35 -0500

    fix(WebController.java): replace field injection with constructor injection and simplify return statements

--- product-ms/src/main/java/com/nexeum/productms/controller/WebController.java
+++ product-ms/src/main/java/com/nexeum/productms/controller/WebController.java
@@ -1,21 +1,22 @@
 package com.nexeum.productms.controller;
 
 import com.nexeum.productms.service.ProductService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/web/product")
+@RequestMapping("/api/v1/web/product")
 public class WebController {
-    @Autowired
-    private ProductService productService;
+    private final ProductService productService;
+
+    public WebController(ProductService productService) {
+        this.productService = productService;
+    }
 
     @GetMapping("/all-products")
     public ResponseEntity<?> getAllProducts() {
-        ResponseEntity<?> response = productService.getAllProducts();
-        return response;
+        return productService.getAllProducts();
     }
 }
