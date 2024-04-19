82bb9c470aa29f3c06d4ce0f56b72d806351bbb4
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 16:17:57 -0500

    fix(dto): fix service response values

--- order-ms/dto/response/ServiceResponse.cs
+++ order-ms/dto/response/ServiceResponse.cs
@@ -1,10 +1,8 @@
-using System;
-
-namespace OrderMs.Dto.Response
+namespace order_ms.dto.response
 {
     public class ServiceResponse
     {
-        public string Code { get; set; }
-        public string Message { get; set; }
+        public string? Code { get; set; } = string.Empty;
+        public string? Message { get; set; } = string.Empty;
     }
 }
