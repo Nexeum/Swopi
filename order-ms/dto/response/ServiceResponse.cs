6ee2e5469bf77c5b6870d68393870158f0baffec
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 10:11:52 -0500

    feat: add response DTOs for order operations

---
+++ order-ms/dto/response/ServiceResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace OrderMs.Dto.Response
+{
+    public class ServiceResponse
+    {
+        public string Code { get; set; }
+        public string Message { get; set; }
+    }
+}
