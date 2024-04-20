7f6a6dd919968cf92e6dfb377df7c1f8ca48d281
Nexeum <juaandhe@bancolombia.com.co>
2024-04-20 09:12:00 -0500

    fix(RequestValidationService.java): add service stereotype

--- auth-ms/src/main/java/com/nexeum/authms/service/RequestValidationService.java
+++ auth-ms/src/main/java/com/nexeum/authms/service/RequestValidationService.java
@@ -2,12 +2,14 @@ package com.nexeum.authms.service;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class RequestValidationService {
     public ResponseEntity<?> requestValidator(BindingResult result) {
         if(result.hasErrors()){
