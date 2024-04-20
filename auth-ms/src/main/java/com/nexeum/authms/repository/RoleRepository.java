9b17e426fcb81dcc23b39fe88d737a9d3f787432
Nexeum <juaandhe@bancolombia.com.co>
2024-04-19 21:35:31 -0500

    feat: add repository user and role

---
+++ auth-ms/src/main/java/com/nexeum/authms/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.nexeum.authms.repository;
+
+import com.nexeum.authms.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findRoleByName(String name);
+}
